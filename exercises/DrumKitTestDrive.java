3a7a8e41f61ae33603eb16c53bb6802b2523b33a
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-08-04 11:29:03 +0100

    Add new exercise

--- exercises/DrumKitTestDrive.java
+++ exercises/DrumKitTestDrive.java
@@ -1,18 +1,3 @@
-class DrumKit {
-
-  boolean topHat = true;
-  boolean snare = true;
-
-  void playSnare() {
-    System.out.println("bang bang ba-bang");
-
-  };
-
-  void playTopHat() {
-    System.out.println("ding ding da-ding");
-  };
-}
-  
   class DrumKitTestDrive {;
   
   public static void main (String[] args) {
