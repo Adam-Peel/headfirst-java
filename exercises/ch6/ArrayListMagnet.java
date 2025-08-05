725b84c3dc6b14d81a05f8437f3ef8a975b09a1a
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-08-05 17:03:57 +0100

    Complete magnet exercise ch 6

---
+++ exercises/ch6/ArrayListMagnet.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class ArrayListMagnet { 
+
+public static void main(String[] args) {
+  ArrayList<String> a = new ArrayList<String>();
+  a.add(0, "zero");
+  a.add(1, "one");
+  a.add(2, "two");
+  a.add(3, "three");
+printList(a);
+
+//2nd print
+if (a.contains("three")) {
+  a.add("four");
+}
+a.remove(2);
+printList(a);
+
+// 3rd print
+if (a.indexOf("four") != 4) {
+a.add(4, "4.2");
+}
+printList(a);
+
+if (a.contains("two")) {
+      a.add("2.2");
+    }
+    printList(a);
+
+
+}
+
+
+public static void printList(ArrayList<String> list) {
+  for (String element : list) {
+    System.out.print(element + " ");
+  }
+  System.out.println();
+}
+}
