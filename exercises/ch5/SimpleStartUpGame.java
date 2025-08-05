87b99019846e17ad0b1964c74c6bc6ba61393f47
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-08-05 10:44:51 +0100

    Add compiled class files for simple game implementation

---
+++ exercises/ch5/SimpleStartUpGame.java
@@ -0,0 +1,27 @@
+public class SimpleStartUpGame {
+
+public static void main (String [] args) {
+    int numOfGuesses = 0;
+    GameHelper helper = new GameHelper();
+    SimpleStartup theStartUp = new SimpleStartup();
+    int randomNum = (int) (Math.random() * 5);
+
+    int [] locations = {randomNum, randomNum + 1, randomNum +2};
+    theStartUp.setLocationCells(locations);
+
+    boolean isAlive = true;
+
+    while (isAlive) {
+      int guess = helper.getUserInput("enter a number");
+      String result = theStartUp.checkYourself(guess);
+      numOfGuesses++;
+      if (result.equals("kill")) {
+        isAlive = false;
+        System.out.println("You took " + numOfGuesses + " guesses");
+      }
+    }
+
+
+  }
+
+}
