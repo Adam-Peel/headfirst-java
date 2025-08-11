26e22acc0eaf6bd39dad731244c39720a0bcaccd
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-08-11 10:42:46 +0100

    Extend to add color change on click

--- exercises/ch14/SimpleGui1.java
+++ exercises/ch14/SimpleGui1.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.event.*;
 
 public class SimpleGui1 implements ActionListener {
   private JButton button;
+  private JFrame frame;
 
   public static void main(String[] args) {
     SimpleGui1 gui = new SimpleGui1();
@@ -11,18 +14,21 @@ public class SimpleGui1 implements ActionListener {
 
   public void go() {
     
-    JFrame frame = new JFrame();
+    frame = new JFrame();
     button = new JButton("click me");
+    MyDrawPanel panel = new MyDrawPanel();
 
     button.addActionListener(this);
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(button);
+    frame.getContentPane().add(BorderLayout.SOUTH, button);
+    frame.getContentPane().add(BorderLayout.CENTER, panel);
     frame.setSize(300, 300);
     frame.setVisible(true);
   }
 
   public void actionPerformed (ActionEvent event) {
+    frame.getContentPane().repaint();
     button.setText("I've been clicked!");
   }
 }
